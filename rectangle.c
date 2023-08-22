1c06491dca9ee2cc917ea888d93b214c73507a94
parth00008 <142876015+parth00008@users.noreply.github.com>
2023-08-22 23:17:33 +0530

    Add files via upload

---
+++ rectangle.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+main()
+
+{
+	int l,w,rectangle;
+	printf("enter the length and width:",l,w);
+	scanf("%d%d",&l,&w);
+	
+	rectangle=l*w;
+	
+	printf("rectangle:%d",rectangle);
+}
